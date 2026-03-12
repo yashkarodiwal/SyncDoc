4511b3b5108c1c0c30b60e2c0233a754c9e3f98f
Yash Karodiwal <yashkarodiwal03@gmail.com>
2026-03-12 14:29:20 +0530

    Update login API endpoint to production URL

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -17,7 +17,7 @@ export default function Login() {
         try {
 
             const res = await axios.post(
-                "http://localhost:5000/api/auth/login",
+                "https://syncdoc.onrender.com/api/auth/login",
                 { email, password }
             );
 
